57d48d045a180511a4ef32d0d24363f76c2c5769
Badduck <badduckssb@gmail.com>
2022-02-26 00:26:14 +0100

    changes

--- KeyboardController.gd
+++ KeyboardController.gd
@@ -3,16 +3,18 @@ extends Node2D
 var _column = 1
 var _row = 1
 
-#5757 lines
-var words = "res://words.txt"
+#12972 lines
+var words = "res://allwords.txt"
+#2315 lines
+var solutions = "res://solutions.txt"
 
 var _cur_word
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-	var _rand_index = randi() % 5756
+	var _rand_index = randi() % 2315
 	var f = File.new()
-	f.open(words, File.READ)
+	f.open(solutions, File.READ)
 	for _i in range(0, _rand_index):
 		f.get_line()
 	_cur_word = f.get_line().to_upper()
@@ -25,11 +27,12 @@ func _check_square_correct(_c):
 			get_node("ViewportContainer/Keyboard/TopRows/" + panel.get_child(0).text).modulate = Color(0.4, 1, 0.4)
 		else:
 			get_node("ViewportContainer/Keyboard/BottomRow/" + panel.get_child(0).text).modulate = Color(0.4, 1, 0.4)
+		return true
 	elif (panel.get_child(0).text in _cur_word):
 		panel.modulate = Color(1, 1, 0.4)
 		if(get_node_or_null("ViewportContainer/Keyboard/TopRows/" + panel.get_child(0).text) != null && get_node("ViewportContainer/Keyboard/TopRows/" + panel.get_child(0).text).modulate == Color(1, 1, 1)):
 			get_node("ViewportContainer/Keyboard/TopRows/" + panel.get_child(0).text).modulate = Color(1, 1, 0.4)
-		elif (get_node("ViewportContainer/Keyboard/BottomRow/" + panel.get_child(0).text).modulate == Color(1, 1, 1)):
+		elif (get_node_or_null("ViewportContainer/Keyboard/BottomRow/" + panel.get_child(0).text) != null && get_node("ViewportContainer/Keyboard/BottomRow/" + panel.get_child(0).text).modulate == Color(1, 1, 1)):
 			get_node("ViewportContainer/Keyboard/BottomRow/" + panel.get_child(0).text).modulate = Color(1, 1, 0.4)
 	else:
 		panel.modulate = Color(0.4, 0.4, 0.4)
@@ -37,6 +40,7 @@ func _check_square_correct(_c):
 			get_node("ViewportContainer/Keyboard/TopRows/" + panel.get_child(0).text).modulate = Color(0.4, 0.4, 0.4)
 		else:
 			get_node("ViewportContainer/Keyboard/BottomRow/" + panel.get_child(0).text).modulate = Color(0.4, 0.4, 0.4)
+	return false
 
 func _remove_letter():
 	if _column > 1:
@@ -47,13 +51,17 @@ func _submit():
 	if (get_node("ViewportContainer/InputFields/" + str(_row) + ", 5").get_child(0).text != ""):
 		var inp = get_node("ViewportContainer/InputFields/" + str(_row) + ", " + str(1)).get_child(0).text + get_node("ViewportContainer/InputFields/" + str(_row) + ", " + str(2)).get_child(0).text + get_node("ViewportContainer/InputFields/" + str(_row) + ", " + str(3)).get_child(0).text + get_node("ViewportContainer/InputFields/" + str(_row) + ", " + str(4)).get_child(0).text + get_node("ViewportContainer/InputFields/" + str(_row) + ", " + str(5)).get_child(0).text
 		if (word_listed(inp)):
-			_check_square_correct(1)
-			_check_square_correct(2)
-			_check_square_correct(3)
-			_check_square_correct(4)
-			_check_square_correct(5)
-			_row += 1
-			_column = 1
+			var correct_amt = 0
+			for i in 5:
+				if _check_square_correct(i + 1):
+					correct_amt += 1
+			if correct_amt == 5:
+				victory()
+			else:
+				_row += 1
+				_column = 1
+				if _row > 6:
+					game_over()
 		else:
 			error("word \"" + inp + "\" not in word list")
 	else:
@@ -63,7 +71,7 @@ func word_listed(var input):
 	input = input.to_lower()
 	var f = File.new()
 	f.open(words, File.READ)
-	for i in range(0, 5757):
+	for i in range(0, 12972):
 		if(input == f.get_line()):
 			return true
 	return false
@@ -72,9 +80,21 @@ func error(var msg):
 	get_node("ViewportContainer/PopupDialog/Label").text = msg
 	get_node("ViewportContainer/PopupDialog").popup()
 
+func game_over():
+	var dlg = get_node("ViewportContainer/EndgameDialog")
+	dlg.dialog_text = "You ran out of guesses.\n The correct word was: " + _cur_word
+	dlg.get_ok().text = "Retry"
+	dlg.popup()
+
+func victory():
+	var dlg = get_node("ViewportContainer/EndgameDialog")
+	dlg.dialog_text = "You guessed the word correctly!\n The word was: " + _cur_word
+	dlg.get_ok().text = "New game"
+	dlg.popup()
+
 #keyboard handling
 func _on_key_pressed(var c):
-	if _column <= 5:
+	if _column <= 5 && _row <= 6:
 		get_node("ViewportContainer/InputFields/" + str(_row) + ", " + str(_column)).get_child(0).text = c
 		_column += 1
 
@@ -83,3 +103,7 @@ func _on_ENTER_pressed():
 func _on_DELETE_pressed():
 	_remove_letter()
 
+
+
+func _restart():
+	get_tree().reload_current_scene()
